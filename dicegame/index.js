75aa1578d43fa0860cefe716e2db3bbc0ee7cb38
alizeeshan1234 <93642091+alizeeshan1234@users.noreply.github.com>
2023-05-22 21:42:47 +0530

    Update index.js

--- dicegame/index.js
+++ dicegame/index.js
@@ -1,36 +1,38 @@
-//player 1 
-const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector('button').addEventListener('click', function () {
 
-const randomDiceImage1 = "dice" + randomNumber1 + ".png";
+    //player 1 
+    const randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-const randomImageSource1 = "images/" + randomDiceImage1;
+    const randomDiceImage1 = "dice" + randomNumber1 + ".png";
 
-const image1 = document.querySelectorAll("img")[0];
+    const randomImageSource1 = "images/" + randomDiceImage1;
 
-image1.setAttribute("src", randomImageSource1);
+    const image1 = document.querySelectorAll("img")[0];
 
-//player 2 
-const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    image1.setAttribute("src", randomImageSource1);
 
-const randomDiceImage2 = "dice" + randomNumber2 + ".png";
+    //player 2 
+    const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-const randomImageSource2 = "images/" + randomDiceImage2;
+    const randomDiceImage2 = "dice" + randomNumber2 + ".png";
 
-const image2 = document.querySelectorAll("img")[1];
+    const randomImageSource2 = "images/" + randomDiceImage2;
 
-image2.setAttribute("src", randomImageSource2);
+    const image2 = document.querySelectorAll("img")[1];
 
-//if
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = " 🚩 Player 1 Wins";
-    document.body.style.backgroundColor = "#827397";
-}
-else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = " 🚩 Player 2 Wins";
-      document.body.style.backgroundColor = "#4D4C7D";
-}
-else {
-    document.querySelector("h1").innerHTML = "Draw";
-    document.body.style.backgroundColor = "#363062";
-    
-}
+    image2.setAttribute("src", randomImageSource2);
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = " 🚩 Player 1 Wins";
+        document.body.style.backgroundColor = "#827397";
+    }
+    else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = " 🚩 Player 2 Wins";
+        document.body.style.backgroundColor = "#4D4C7D";
+    }
+    else {
+        document.querySelector("h1").innerHTML = "Draw";
+        document.body.style.backgroundColor = "#363062";
+    }
+
+});
